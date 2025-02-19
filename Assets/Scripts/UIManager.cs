28f88247c31b4fcb8c70c5b462f59bd997952336
Beyza Nur Keskin <byzanur.keskin001@gmail.com>
2025-02-19 18:19:48 +0300

    [Update] Add: UI
    Add UI Manager scrpit and some images for ui.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text _scoreText;
+
+    [SerializeField]
+    private Image _livesImg;
+
+    [SerializeField]
+    private Sprite[] _liveSprites;
+
+    [SerializeField]
+    private Text _gameoverText;
+
+    void Start()
+    {
+        _scoreText.text = "Score: " + 0;
+        _gameoverText.gameObject.SetActive(false);
+    }
+
+    public void UpdateScore(int playerScore)
+    {
+        _scoreText.text = "Score: " + playerScore.ToString();
+    }
+
+    public void UpdateLives(int currentLives)
+    {
+        _livesImg.sprite = _liveSprites[currentLives];
+        if(currentLives == 0)
+        {
+            _gameoverText.gameObject.SetActive(true);
+            StartCoroutine(GameOverFlickerRoutine());
+        }
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while (true) 
+        {
+            _gameoverText.text = "GAME OVER";
+            yield return new WaitForSeconds(0.5f);
+            _gameoverText.text = "";
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+    }
+}
