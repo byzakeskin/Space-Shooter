6fcda106c88207a0a5202c42845a9709a3ac2d78
Beyza Nur Keskin <byzanur.keskin001@gmail.com>
2025-02-19 13:29:53 +0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+
+    private float _speedPowerup = 2f;
+
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+    [SerializeField]
+    private GameObject _tripleLaserPrefab;
+
+    [SerializeField]
+    private float _fireRate = 0.15f;
+
+    private float _canFire = -1f;
+
+    [SerializeField]
+    private int _lives = 3;
+
+    private SpawnManager _spawnManager;
+ 
+    private bool _isTripleLaserActive = false;
+    private bool _isSpeedPowerupActive = false;
+
+
+    void Start()
+    {
+        transform.position = new Vector3(0, -3, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+
+        if(_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is null!");
+        }
+    }
+
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            ShootLaser();
+        }
+            
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        
+        //Mathf.Clamp allows setting min and max values ​​so we can easily draw the boundaries.
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+        
+        if(_isSpeedPowerupActive == false)
+        {
+            transform.Translate(direction * _speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(direction * _speed * _speedPowerup * Time.deltaTime);
+        }
+
+        if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+    }
+
+    void ShootLaser()
+    {              
+        _canFire = Time.time * _fireRate;
+        if (_isTripleLaserActive == true)
+        {
+            Instantiate(_tripleLaserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        }
+    }
+
+    public void Damage()
+    {
+        //All these writing styles serve the same function.
+        //_lives --;
+        //_lives = _lives - 1;
+        _lives --;
+
+        if(_lives < 1)
+        {
+            _spawnManager.OnPlayerDEath(); 
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void TripleLaserActive()
+    {
+        _isTripleLaserActive = true;
+        StartCoroutine(TripleLaserPowerDownRoutine());
+    }
+
+    IEnumerator TripleLaserPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+        _isTripleLaserActive = false;
+    }
+
+    public void SpeedPowerupActive()
+    {
+        _isSpeedPowerupActive = true;
+        StartCoroutine(SpeedPowerUPPowerDownRoutine());
+    }
+
+    IEnumerator SpeedPowerUPPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+        _isSpeedPowerupActive = false;
+    }
+}
+
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+
+    [SerializeField]
+    private GameObject _enemyContainer;
+
+    [SerializeField]
+    private GameObject _powerupPrefab;
+
+    [SerializeField]
+    private GameObject _speedpowerupPrefab;
+
+    private bool _stopSpawning = false;
+
+    void Start()
+    {
+        //Allows us to perform timer-controlled operations.
+        StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnPowerUpRoutine());
+        StartCoroutine(SpawnPowerUpSpeedRoutine());
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 postoSpwan = new Vector3(Random.Range(-7f, 7f),7 ,0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, postoSpwan, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(5.0f);
+        } 
+    }
+
+    IEnumerator SpawnPowerUpRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 postoSpwan = new Vector3(Random.Range(-7f, 7f), 7, 0);
+            GameObject newPowerUP = Instantiate(_powerupPrefab, postoSpwan, Quaternion.identity);
+            yield return new WaitForSeconds(15.0f);
+        }
+    }
+
+    IEnumerator SpawnPowerUpSpeedRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 postoSpwan = new Vector3(Random.Range(-7f, 7f), 7, 0);
+            GameObject newPowerUP = Instantiate(_speedpowerupPrefab, postoSpwan, Quaternion.identity);
+            yield return new WaitForSeconds(15.0f);
+        }
+    }
+
+
+    public void OnPlayerDEath()
+    {
+        _stopSpawning = true;
+    }
+}
