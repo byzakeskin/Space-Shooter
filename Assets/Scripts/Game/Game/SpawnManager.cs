8b6e0ab15ae67c305a612866336d13ff53947113
Beyza Nur Keskin <byzanur.keskin001@gmail.com>
2025-02-19 20:26:24 +0300

    [Assets]Update: Scripts

---
+++ Assets/Scripts/Game/Game/SpawnManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+
+    [SerializeField]
+    private GameObject _enemyContainer;
+
+    [SerializeField]
+    private GameObject _powerupPrefab;
+
+    [SerializeField]
+    private GameObject _speedpowerupPrefab;
+
+    [SerializeField]
+    private GameObject _shieldpowerupPrefab;
+
+    private bool _stopSpawning = false;
+
+    void Start()
+    {
+        //Allows us to perform timer-controlled operations.
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerUpTripleLaserRoutine());
+        StartCoroutine(SpawnPowerUpSpeedRoutine());
+        StartCoroutine(SpawnPowerUpShieldRoutine());
+    }
+
+    IEnumerator SpawnEnemyRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 postoSpwan = new Vector3(Random.Range(-7f, 7f),7 ,0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, postoSpwan, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(5.0f);
+        } 
+    }
+
+    IEnumerator SpawnPowerUpTripleLaserRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 postoSpwan = new Vector3(Random.Range(-7f, 7f), 7, 0);
+            GameObject newPowerUP = Instantiate(_powerupPrefab, postoSpwan, Quaternion.identity);
+            yield return new WaitForSeconds(15.0f);
+        }
+    }
+
+    IEnumerator SpawnPowerUpSpeedRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 postoSpwan = new Vector3(Random.Range(-7f, 7f), 7, 0);
+            GameObject newPowerUP = Instantiate(_speedpowerupPrefab, postoSpwan, Quaternion.identity);
+            yield return new WaitForSeconds(15.0f);
+        }
+    }
+
+    IEnumerator SpawnPowerUpShieldRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 postoSpwan = new Vector3(Random.Range(-3f, 3f), 7, 0);
+            GameObject newPowerUP = Instantiate(_shieldpowerupPrefab, postoSpwan, Quaternion.identity);
+            yield return new WaitForSeconds(30.0f);
+        }
+    }
+
+
+    public void OnPlayerDEath()
+    {
+        _stopSpawning = true;
+    }
+}
---
+++ Assets/Scripts/Game/Main_Menu/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        //set main menu index = 0 rom build settings
+        SceneManager.LoadScene(1); //load game scene
+    }
+}
